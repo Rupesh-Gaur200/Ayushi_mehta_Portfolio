83af1d372c9d1528ff34636a5b5b5b970162fd08
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-04-06 20:33:22 +0530

    Created the Navbar Componenent

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,9 +2,9 @@
 function Navbar(){
     return(
    
-        <div className="px-20 py-6 border-2 ">
-            <div className="w-full h-full flex justify-between items-center">
-            <h1 className="text-5xl text-[#eaeaea] font-['Licorice']">Ayushi Mehta</h1>
+        <div className="px-20 py-6 ">
+            <div className="w-[1350px] h-full flex justify-between items-center">
+            <h1 className="text-5xl text-[#eaeaea] font-['Licorice'] font-bold">Ayushi Mehta</h1>
             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="jss67" aria-label="Menu" height="40px" width="40px" xmlns="http://www.w3.org/2000/svg"><path d="M64 384h384v-42.67H64zm0-106.67h384v-42.66H64zM64 128v42.67h384V128z"></path></svg>
             </div>
         </div>
