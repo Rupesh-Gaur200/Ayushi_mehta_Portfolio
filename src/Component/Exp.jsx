ffcde2ed92534789265c2089ae95259fb1b34155
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-04-07 18:34:25 +0530

    Created Experince Component to Showcase Experince

---
+++ src/Component/Exp.jsx
@@ -0,0 +1,82 @@
+
+
+function Exp(){
+    const educationData=[{
+        date:"Aug 2023- Present",
+        Role:"Digital Marketing Executive ",
+        Organ:"Intellipaat Software Solutions Pvt. Ltd.",
+
+    },
+
+    {
+        date:"july 2023- Aug 2023",
+        Role:"General Secretary",
+        Organ:"Synopsis",
+
+    },
+    
+
+
+
+
+]
+
+ return(
+    <div className="px-20 py-16 flex gap-10 ">
+        <img className="w-[600px] h-[500px]" src="https://yashi-gupta.netlify.app/static/media/expRed.1bdca5f9.svg"></img>
+    <div className=" w-[100%] h-full flex flex-col gap-[12px]">
+    <h1 className="font-bold text-6xl text-[#f03939] ml-[61%]">Experience</h1>
+
+    <div className="py-10 px-10 gap-10 flex flex-col ">
+
+       {educationData.map((item ,index)=>{
+           return(
+               
+       <div key={index} className="bg-[#f38a8a]  flex gap-4 h-[100px] items-center pl-5 rounded-xl" >
+
+       <div className="bg-[#f03939] rounded-full flex items-center h-[65%] px-3">
+          <img className="" src="https://yashi-gupta.netlify.app/static/media/expImgBlack.01d2dd8e.svg"></img>
+
+       </div>
+
+        <div>
+        <h6 className="color- [rgb(240, 57, 57)] font-ligthbold ">{item.date}</h6>
+        <h4 className="color-[rgb(33, 33, 33)] font-bold">{item.Role}</h4>
+        <h5 className="color-[rgba(33, 33, 33, 0.8)] font-semibold">{item.Organ}</h5>
+        </div>
+
+      </div>
+           )
+       })}
+
+       {/* <div className="bg-[#f38a8a]  flex gap-4 h-[100px] items-center pl-5 rounded-xl" >
+
+        <div className="bg-[#f03939] rounded-full flex items-center h-[65%] ">
+           <img className="" src="https://yashi-gupta.netlify.app/static/media/eduImgBlack.8ee2e691.svg"></img>
+
+        </div>
+
+         <div>
+         <h6 className="color- [rgb(240, 57, 57)] font-ligthbold "></h6>
+         <h4 className="color-[rgb(33, 33, 33)] font-bold"></h4>
+         <h5 className="color-[rgba(33, 33, 33, 0.8)] font-semibold"></h5>
+         </div>
+
+       </div> */}
+       </div>
+    </div>
+ 
+   
+    
+
+   </div>
+   
+)
+}
+
+
+ 
+
+
+
+export default Exp
