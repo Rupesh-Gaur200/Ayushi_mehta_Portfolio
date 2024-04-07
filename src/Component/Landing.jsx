cc5cfa613b7c522eb0e5c14d5249025038ffdaad
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-04-07 14:10:55 +0530

    Created the who I am Section

--- src/Component/Landing.jsx
+++ src/Component/Landing.jsx
@@ -2,9 +2,33 @@ import Navbar from "./Navbar"
 function Landing(){
 
     return(
-        <div>
+        <div className="px-20 py-28">
        
-        <h1>Hello Landing</h1>
+         <div className="flex gap-2">
+          
+           <div className="flex flex-col gap-20">
+            <h1 className="text-6xl text-[#f03939]  font-bold">
+            Who I am
+            </h1>
+            <div className="w-[75%] flex flex-col gap-8 ">
+            <p className="text-wrap font-semibold text-xl italic text-[#444444]">
+            Hi ! I'm Yashi Gupta, An Engineer And A Developer. I'am pursuing B.Tech Computer Science and Engineering  from Lovely Professional University , doing my minors in Web Development and Android Development .I am a web development freelancer and like to explore different niches over time. Having experience in website development, event management and research and development.
+            </p>
+            <p className="text-wrap font-semibold text-xl italic text-[#444444]">
+            I always believe in being creative and unique , seeking for opportunities to learn something new.I have also worked upon many live projects. My keen interest includes web-development, website designing, competitive programming, cloud computing, and coding . Looking for more oppurtunities to work upon!
+            </p>
+            </div>
+          
+
+           
+           </div>
+
+          <img className="h-[580px]" src="https://yashi-gupta.netlify.app/static/media/redTheme-girl.e2b9dd83.svg"></img>
+
+
+
+
+         </div>
       
        
           
