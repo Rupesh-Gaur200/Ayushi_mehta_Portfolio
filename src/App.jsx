7eed8dc6981826c29bc55f0bf4b612680e61f144
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-04-07 13:16:11 +0530

    Completed the Layout of Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 
 import Landing from "./Component/Landing"
+import LeftBottomitem from "./Component/LeftBottomitem"
 import Navbar from "./Component/Navbar"
+import RightSidehead from "./Component/RightSidehead"
 
 function App() {
 
 
   return (
     <div>
-    <div className="bg-[#eaeaea] w-full ">
+    <div className=" relative bg-[#eaeaea] w-full ">
     
-       <div className="w-[35%] bg-red-500 h-screen ">
+       <div className=" w-[35%] bg-red-500 h-screen ">
        <Navbar></Navbar>
-       <img className="w-[400px] rounded-full mx-80 mt-14"src="https://media.licdn.com/dms/image/D5635AQHoXazWNdvNMQ/profile-framedphoto-shrink_800_800/0/1712139634735?e=1713024000&v=beta&t=cs8tTWYm47FCbtZyJZdHsFuzRHSumlGElnwmbNgwf_4"></img>
-
+       <img className="w-[370px] rounded-full mx-[360px] mt-20"src="https://media.licdn.com/dms/image/D5635AQHoXazWNdvNMQ/profile-framedphoto-shrink_800_800/0/1712139634735?e=1713024000&v=beta&t=cs8tTWYm47FCbtZyJZdHsFuzRHSumlGElnwmbNgwf_4"></img>
+          
+            <LeftBottomitem></LeftBottomitem>
             </div>
-
+            <RightSidehead></RightSidehead>
             <Landing></Landing>
            
          
